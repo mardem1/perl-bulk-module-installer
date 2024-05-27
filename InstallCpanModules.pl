5d9bea69e16d079f457f9191fe16135920826df8
Markus Demml <104694616+mardem1@users.noreply.github.com>
2024-05-27 16:55:02 +0200

    doc update

--- InstallCpanModules.pl
+++ InstallCpanModules.pl
@@ -1174,11 +1174,18 @@ __END__
 
 =head1 NAME
 
-InstallCpanModules.pl
+InstallCpanModules.pl filepath
 
 =head1 DESCRIPTION
 
 TODO
+
+=head1 BUG REPORTS
+
+Please report bugs on GitHub.
+
+The source code repository can be found
+at https://github.com/mardem1/perl-bulk-module-installer
 
 =head1 AUTHOR
 
@@ -1192,5 +1199,13 @@ This library is free software; you can redistribute it and/or modify it
 under the same terms as the Perl 5 programming language system itself.
 The full text of this license can be found in the LICENSE file included
 with this module.
+
+=head1 DISCLAIMER
+
+Copyright (c) 2024, Markus Demml
+
+This package is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
 
 =cut
