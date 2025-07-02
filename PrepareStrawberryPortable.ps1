1a0366b5a8b1a29291595a121cd68b3a614bffe8
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-02 11:33:31 +0200

    PrepareStrawberryPortable.ps1 started with vision definition.

---
+++ PrepareStrawberryPortable.ps1
@@ -0,0 +1,58 @@
+﻿
+<#
+
+.SYNOPSIS
+
+Extracts the Strawberry ZIP and configure Windows Defender for more performance.
+
+.PARAMETER StrawberryZip
+
+Path to Strawberry ZIP portable.
+
+.NOTES
+
+BUG REPORTS
+
+Please report bugs on GitHub.
+
+The source code repository can be found
+at https://github.com/mardem1/perl-bulk-module-installer
+
+AUTHOR
+
+Markus Demml, mardem@cpan.com
+
+LICENSE AND COPYRIGHT
+
+Copyright (c) 2025, Markus Demml
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as the Perl 5 programming language system itself.
+The full text of this license can be found in the LICENSE file included
+with this module.
+
+DISCLAIMER
+
+This package is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path -LiteralPath $_ -PathType Leaf })]
+    [string] $StrawberryZip
+)
+
+Write-Host ''
+Write-Host -ForegroundColor Green "started '$($MyInvocation.InvocationName)' ..."
+Write-Host ''
+
+Write-Host ''
+Write-Host -ForegroundColor Green 'done'
+Write-Host ''
+Write-Host -ForegroundColor Green "... '$($MyInvocation.InvocationName)' ended"
+Write-Host ''
