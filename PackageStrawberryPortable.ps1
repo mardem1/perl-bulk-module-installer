4ebde05a18f0d5e557833f8680f8f516e59de217
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-02 15:43:47 +0200

    PackageStrawberryPortable.ps1 as to implement stub

---
+++ PackageStrawberryPortable.ps1
@@ -0,0 +1,61 @@
+﻿
+<#
+
+.SYNOPSIS
+
+Remove Windows Defender exclusions, do a manual defender scan and creates a ZIP for the Strawberry directory.
+
+.PARAMETER StrawberryDir
+
+Path to Strawberry directory for zipping
+
+.NOTES
+
+BUG REPORTS
+
+Please report bugs on GitHub.
+
+The source code repository can be found
+at https://github.com/mardem1/perl-bulk-module-installer
+
+AUTHOR
+
+Markus Demml, mardem@cpan.com
+
+LICENSE AND COPYRIGHT
+
+Copyright (c) 2025, Markus Demml
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as the Perl 5 programming language system itself.
+The full text of this license can be found in the LICENSE file included
+with this module.
+
+DISCLAIMER
+
+This package is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path -LiteralPath $_ -PathType Container })]
+    [ValidateScript({ $_ -like '*strawberry*portable*' })]
+    [string] $StrawberryDir
+)
+
+Write-Host ''
+Write-Host -ForegroundColor Green "started '$($MyInvocation.InvocationName)' ..."
+Write-Host ''
+
+# TODO: only stub here ...
+
+Write-Host ''
+Write-Host -ForegroundColor Green 'done'
+Write-Host ''
+Write-Host -ForegroundColor Green "... '$($MyInvocation.InvocationName)' ended"
+Write-Host ''
