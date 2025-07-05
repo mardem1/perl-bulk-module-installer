7dd6bc64fd67f446f496814897b604cf6ef1507f
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-05 19:23:10 +0200

    test-sequence.ps1 renamed, corrected and extracted portableBaseName local var

--- _util/test-sequence.ps1
+++ _util/test-sequence.ps1
@@ -1,10 +1,16 @@
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_a_Prepare.ps1" -StrawberryZip "C:\_test\strawberry-perl-5.40.2.1-64bit-portable.zip"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_before.txt"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_b_InstallModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SingleModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_b_InstallModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SmallModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_b_InstallModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SingleModuleExample.txt", "C:\_test\perl-bulk-module-installer\test-module-lists\SmallModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_after.txt"
-& "C:\_test\perl-bulk-module-installer\StrawberryPortable_d_Package.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.40.2.1-64bit-portable"
+
+$portableBaseName = 'strawberry-perl-5.40.2.1-64bit-portable'
+
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_a_Extract.ps1" -StrawberryZip "C:\_test\$portableBaseName.zip" -Destination "C:\_test\$portableBaseName"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_b_AddDefenderExclude.ps1" -StrawberryDir "C:\_test\$portableBaseName"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\$portableBaseName" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_before.txt"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_d_InstallModules.ps1" -StrawberryDir "C:\_test\$portableBaseName" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SingleModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_d_InstallModules.ps1" -StrawberryDir "C:\_test\$portableBaseName" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SmallModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_d_InstallModules.ps1" -StrawberryDir "C:\_test\$portableBaseName" -InstallModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\SingleModuleExample.txt", "C:\_test\perl-bulk-module-installer\test-module-lists\SmallModuleExample.txt" -DontTryModuleListFile "C:\_test\perl-bulk-module-installer\test-module-lists\_dont_try_modules.txt"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\$portableBaseName" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_after.txt"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_e_RemoveDefenderExclude.ps1" -StrawberryDir "C:\_test\$portableBaseName"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_f_RunDefenderScan.ps1" -StrawberryDir "C:\_test\$portableBaseName"
+& "C:\_test\perl-bulk-module-installer\StrawberryPortable_d_Package.ps1" -StrawberryDir "C:\_test\$portableBaseName"
 
 & "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.24.4.1-64bit-portable" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_5.24.4.1.txt"
 & "C:\_test\perl-bulk-module-installer\StrawberryPortable_c_CpanL_ListModules.ps1" -StrawberryDir "C:\_test\strawberry-perl-5.26.3.1-64bit-portable" -ModuleListFileTxt "C:\_test\perl-bulk-module-installer\log\_list_5.26.3.1.txt"
