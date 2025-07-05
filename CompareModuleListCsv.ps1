8794b4b62969368713ce771a33ef30900d1fc1e0
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-05 16:09:14 +0200

    CompareModuleListCsv.ps1 filter module which have different version and save it to a cvs file.

--- CompareModuleListCsv.ps1
+++ CompareModuleListCsv.ps1
@@ -12,6 +12,10 @@ First list
 
 Second list
 
+.PARAMETER CompareResultList
+
+csv file with all direrences
+
 .NOTES
 
 BUG REPORTS
@@ -54,7 +58,13 @@ param (
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-Path -LiteralPath $_ -PathType Leaf })]
     [ValidateScript({ $_ -like '*.csv' })]
-    [string] $ListB
+    [string] $ListB,
+
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({ Test-Path -LiteralPath $_ -PathType Leaf -IsValid })]
+    [ValidateScript({ $_ -like '*.csv' })]
+    [string] $CompareResultList
 )
 
 $ScriptPath = $MyInvocation.InvocationName
@@ -72,7 +82,94 @@ Write-Host ''
 Write-Host -ForegroundColor Green "started '$ScriptPath' ..."
 Write-Host ''
 
-# TODO: implement
+[hashtable]$combinedModules = @{}
+
+$allLists = @()
+
+$lists = @{
+    "$ListA" = 'ListA'
+    "$ListB" = 'ListB'
+}
+
+$ListA, $ListB | ForEach-Object {
+    $file = $_
+
+    Write-Host ''
+    Write-Host -ForegroundColor Green "analyze '$file' ..."
+
+    $lines = Get-Content -LiteralPath $file
+    $list = $lines | Where-Object { $_ -like '# *' } | ForEach-Object {
+        $t = $_.Split(';')
+
+        if ([string]::IsNullOrWhiteSpace($t[0])) {
+            throw "perl title in CSV file not found - $file"
+        }
+
+        if ([string]::IsNullOrWhiteSpace($t[1])) {
+            throw "perl verison in CSV file not found - $file"
+        }
+
+        $t[1]
+    }
+
+    if ($null -eq $list) {
+        throw "perl info in CSV file not found - $file"
+    }
+
+    $list = "$($lists[$file])-$list "
+
+    $allLists += $list
+
+    # https://github.com/PowerShell/PowerShell/blob/744a53a2038056467b6ddeb2045336d79480c4c0/src/Microsoft.PowerShell.Commands.Utility/commands/utility/CsvCommands.cs#L1362
+    # ConvertFrom-Csv - ignores lines which start wiht #
+    $modules = $lines | ConvertFrom-Csv -Delimiter ';' -Header 'Module', 'Version'
+
+    $modules | ForEach-Object {
+        $module = $_
+        if ( ! $combinedModules.ContainsKey($module.Module) ) {
+            $combinedModules[$module.Module] = @{}
+        }
+
+        $combinedModules[$module.Module][$list] = $module.Version
+    }
+}
+
+Write-Host ''
+Write-Host -ForegroundColor Green 'init moudule not found in verison'
+Write-Host ''
+
+$keys = $combinedModules.Keys | Sort-Object -Unique
+$allLists | ForEach-Object {
+    $list = $_
+
+    $keys | ForEach-Object {
+        $module = $_
+        if ( ! $combinedModules.ContainsKey($module) ) {
+            # ignore already deleted
+        }
+        elseif ( $combinedModules[$module].Count -eq 2) {
+            $moduleVersions = $combinedModules[$module].Values | Sort-Object -Unique
+            if ( @($moduleVersions).Count -eq 1 ) {
+                Write-Host -ForegroundColor Green "remove $module - version equal"
+                $combinedModules.Remove($module)
+            }
+        }
+        elseif ( ! $combinedModules[$module].ContainsKey($list)) {
+            $combinedModules[$module][$list] = 'not-installed'
+        }
+    }
+}
+
+$moduleLines = $combinedModules.Keys | Sort-Object -Unique | ForEach-Object {
+    $m = $_
+    $a = $combinedModules[$m]["$($allLists[0])"]
+    $b = $combinedModules[$m]["$($allLists[1])"]
+    "$m;$a;$b"
+}
+
+Write-Host ''
+Write-Host -ForegroundColor Green "write csv file $CompareResultList"
+"Module;$($allLists[0]);$($allLists[1])" , $moduleLines | Out-File -LiteralPath $CompareResultList -Encoding default -Force -Confirm:$false -Width 999
 
 Write-Host ''
 Write-Host -ForegroundColor Green 'done'
