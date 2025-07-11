61b97ca4268c057bad9dd1fe28e3372c2fe9695d
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-11 23:56:37 +0200

    PerlBulkModuleInstaller.pm fixed install_module_with_dependencies_first_recursive mark module with failed, if dependent module isn't ok.

--- lib/PerlBulkModuleInstaller.pm
+++ lib/PerlBulkModuleInstaller.pm
@@ -1392,6 +1392,8 @@ sub install_module_with_dependencies_first_recursive
                     $recursion--;
 
                     if ( $hasError ) {
+                        # mark module as failed, if dependent module not ok
+                        mark_module_as_failed( $module );
                         return $hasError;
                     }
                 }
