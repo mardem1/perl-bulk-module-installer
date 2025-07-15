80cd9095b55bdcd432a149ce7c74fe33a0b98cb1
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-15 20:24:32 +0200

    start_powershell.bat use mainly full path and powershell from path only if not found.

--- _util/start_powershell.bat
+++ _util/start_powershell.bat
@@ -1,1 +1,6 @@
-start powershell -NoProfile -ExecutionPolicy ByPass -NoLogo
+set PS1_FULLPATH=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+set POWERSHELL=powershell
+
+if exist "%PS1_FULLPATH%" set POWERSHELL=%PS1_FULLPATH%
+
+start "powershell" /I "%POWERSHELL%" -NoProfile -ExecutionPolicy ByPass -NoLogo
