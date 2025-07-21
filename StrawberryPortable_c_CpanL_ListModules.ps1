73f301a04a76964ed69fdc34fd03d1665562e09c
Markus Demml <104694616+mardem1@users.noreply.github.com>
2025-07-21 23:41:41 +0200

    trim HW Product Name

--- StrawberryPortable_c_CpanL_ListModules.ps1
+++ StrawberryPortable_c_CpanL_ListModules.ps1
@@ -113,7 +113,7 @@ try {
     $hwModel = "$($wcs.Model) $($wcs.Manufacturer)"
     $hwRam = "$([System.Math]::Round( [double] $wcs.TotalPhysicalMemory / 1GB , 2)) GByte" # ( Get-CimInstance Win32_OperatingSystem ).TotalVisibleMemorySize
     $wp = Get-CimInstance -ClassName Win32_Processor | Sort-Object -Property NumberOfLogicalProcessors -Descending | Select-Object -First 1 # TODO: dual socket system support ?
-    $hwCpu = "$($wp.Name) $($wp.NumberOfCores)C / $($wp.NumberOfLogicalProcessors)T" # ( Get-CimInstance Win32_OperatingSystem ).TotalVisibleMemorySize
+    $hwCpu = "$($($wp.Name).Trim()) ($($wp.NumberOfCores)C / $($wp.NumberOfLogicalProcessors)T)" # ( Get-CimInstance Win32_OperatingSystem ).TotalVisibleMemorySize
     $isVm = if ( $wcs.HypervisorPresent ) {
         'Hypervisor present'
     }
